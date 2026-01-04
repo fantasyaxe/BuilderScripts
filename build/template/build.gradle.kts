30356c7f5098d2ad6badf722cdf7bffd579cb52d
Nionim (C!tory¿) <delta.cion.fe@gmail.com>
2026-01-04 21:24:50 +0300

    Update build.gradle.kts

--- build/template/build.gradle.kts
+++ build/template/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "net.minecraft"
-version = "1.5.2"
+version = "$deVersion"
 
 allprojects {
     apply(plugin = "java")
@@ -12,39 +12,18 @@ allprojects {
 
     java {
         toolchain {
-            languageVersion = JavaLanguageVersion.of(21)
+            languageVersion = JavaLanguageVersion.of($deJavaVersion)
         }
     }
 
     dependencies {
-        // codec
-        implementation("com.jcraft:jogg:0.0.7")
-        implementation("com.jcraft:jorbis:0.0.17")
-        // render
-        implementation("org.lwjgl:lwjgl:3.2.1")
-        implementation("org.lwjgl.lwjgl:lwjgl_util:2.9.1")
-
-        // replaced
-        implementation("net.sourceforge.argo:argo:7.6")
-
-
-        implementation("com.paulscode:codecwav:20101023")
-        implementation("com.paulscode:codecjorbis:20101023")
-        implementation("com.paulscode:soundsystem:201809301515")
-        implementation("com.paulscode:librarylwjglopenal:20100824")
-
-        implementation("org.jdom:jdom:1.1.3")
-        implementation("org.bouncycastle:bcprov-jdk15on:1.47")
-        implementation("org.xerial:sqlite-jdbc:3.7.2")
+        $$dependsImplements
+        $$dependsCompileOnly
     }
 
     repositories {
         mavenCentral()
-        maven("https://maven.minecraftforge.net/")
-        maven("https://www.beatunes.com/repo/maven2/")
-        maven("https://repo.carm.cc/repository/maven-public/")
-        maven("https://repo.glaremasters.me/repository/public/")
-        maven("https://nexus.velocitypowered.com/repository/maven-public/")
+        $$repositories
     }
 
     tasks {
@@ -59,11 +38,11 @@ allprojects {
 
         withType<JavaCompile> {
             options.encoding = "UTF-8"
-            options.release = 21
+            options.release = $deJavaVersion
         }
 
         withType<Jar> {
-            manifest { attributes["Main-Class"] = "net.minecraft.client.Minecraft" }
+            manifest { attributes["Main-Class"] = "$deMainClass" }
         }
     }
 }
